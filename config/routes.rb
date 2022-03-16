04f34c01197ce8c0950dbfee9c3b4fc67916de86
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-03-16 06:07:30 +0000

    Add ジャンル一覧・追加画面作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   namespace :admin do
-    get 'genres/index'
-    get 'genres/edit'
+    resources :genres, except: [:new, :show, :destroy]
   end
+  
   # 顧客用
   # URL /customers/sign_in ...
   devise_for :customers, skip: [:passwords], controllers: {
