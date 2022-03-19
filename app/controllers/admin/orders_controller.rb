58e2b1af1f76a4a17514eb0f00b53eeda414b5d0
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-03-19 08:45:22 +0000

    Add 注文履歴詳細画面

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :shipping_cost, :total_payment, :payment_method)
+  end 
 end
