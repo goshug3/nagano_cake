7308d2d025f07e623a82d1f0dffbd80f761cd3c4
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-03-19 03:33:34 +0000

    Add admin/customers controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :admin do
+    get 'customers/index'
+    get 'customers/show'
+    get 'customers/edit'
+  end
+  namespace :admin do
     resources :genres, except: [:new, :show, :destroy]
     resources :items, except: [:destroy]
   end
