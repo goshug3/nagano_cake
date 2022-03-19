f12b9aa3b4827df26fd193bad2a54c5288ebae03
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-03-19 04:52:55 +0000

    Add 会員一覧画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,5 +1,6 @@
 class Admin::CustomersController < ApplicationController
   def index
+    @customers = Customer.page(params[:page])
   end
 
   def show
@@ -7,4 +8,10 @@ class Admin::CustomersController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def customer_params
+    params.recuire(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :phone_number, :is_active, :email)
+  end 
 end
