152fa4a1785bd2e79f8ce031fa7619819e90f540
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-03-26 08:25:16 +0000

    Add 登録情報編集画面

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,10 +1,25 @@
 class Public::CustomersController < ApplicationController
+
   def show
   end
 
   def edit
+    @customer = current_customer
+  end
+  
+  def update
+    @customer = current_customer
+    redirect_to customers_path
   end
 
   def unsubscribe
   end
+  
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :email, :telephone_number)
+  end
+
 end
+
