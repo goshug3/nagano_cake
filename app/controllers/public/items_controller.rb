329efddb9f8321e3376c7b6c32cd83813bffdf85
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-04-06 16:17:46 +0000

    Add 商品一覧画面

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,14 @@
 class Public::ItemsController < ApplicationController
   def index
+    @genres = Genre.all
+    @items = Item.page(params[:page])
   end
 
   def show
+  end
+  
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :price)
   end
 end
