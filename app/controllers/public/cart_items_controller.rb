be79aa0a500d93a6010955a6c94f4b0de8bddd02
EC2 Default User <ec2-user@ip-172-31-92-198.ec2.internal>
2022-04-06 17:03:18 +0000

    Add カート画面

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,4 +1,38 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
+  
   def index
+    @cart_items = current_customer.cart_items.all
+    @total_price = @cart_items.inject(0) { |sum, item| sum + item.subtotal }
   end
+  
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.save
+    redirect_to items_path, notice: "カートに追加されました"
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update
+    redirect_to public_cart_items_path(current_customer.id)
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path(current_customer.id)
+  end
+
+  def all_destroy
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to cart_items_path(current_customer.id)
+  end
+  
+  private
+  def cart_item_params
+      params.require(:cart_item).permit(:item_id, :customer_id, :amount)
+  end
+  
 end
